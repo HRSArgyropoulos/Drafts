a230c1fd9cd636208c8a6204c72595671d4a1899
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-06-03 15:52:53 +0300

    arrow svg and booking search modifications - swiper

--- Hotel/js/swiper.js
+++ Hotel/js/swiper.js
@@ -7,22 +7,12 @@ const swiper = new Swiper('.swiper-container', {
     fadeEffect: {
       crossFade: true
     },
-    speed: 1000,
-    /* autoplay: {
-      delay: 5000,
+    speed: 2000,
+    autoplay: {
+      delay: 10000,
       disableOnInteraction: false,
-    }, */
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
     },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
+    navigation: false,
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
